fd862dd1085f0eb132a43ed3dd8712408c90c313
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-10 18:47:05 +0000

    maths operations

---
+++ basics/numbers.js
@@ -0,0 +1,20 @@
+const score = 190.846;
+
+const balance = new Number(33);
+// console.log(balance);
+
+// console.log(score.toFixed(4));
+// console.log(score.toPrecision(3));
+
+const hunderds= 10000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+// converts negative into positive
+console.log(Math.abs(-4));
+
+// rounds the number 
+console.log(Math.round(44.7));
+
