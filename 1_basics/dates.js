cf3c64988887176b885e55ed8092e650dbd9bdef
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-11 15:45:18 +0000

    2_basics folder created

---
+++ 1_basics/dates.js
@@ -0,0 +1,15 @@
+let myDate= new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let newDate= new Date(2025, 6, 24);
+console.log(newDate.toLocaleString());
+// console.log(newDate.toLocaleDateString());
+
+let date = new Date()
+console.log(date.getDay());
+
