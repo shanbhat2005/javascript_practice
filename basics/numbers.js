e29c25c8941a70fe1b4dfb1fd79c51333e1715c1
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-11 08:08:42 +0000

    maths operations complete

--- basics/numbers.js
+++ basics/numbers.js
@@ -4,7 +4,7 @@ const balance = new Number(33);
 // console.log(balance);
 
 // console.log(score.toFixed(4));
-// console.log(score.toPrecision(3));
+console.log(score.toPrecision(2));
 
 const hunderds= 10000000
 // console.log(hunderds.toLocaleString('en-IN'));
@@ -18,3 +18,12 @@ console.log(Math.abs(-4));
 // rounds the number 
 console.log(Math.round(44.7));
 
+// to get a number greater than 0
+
+console.log(Math.floor(Math.random()*10)+1);
+
+// to get a number within a range of minimum and maximum number
+let min=10;
+let max= 30;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
