af7bfd3090b375204fe4669a26eefe87f406f59b
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-12 15:25:57 +0000

    first js file topics completed

--- 2_basics/arrays.js
+++ 2_basics/arrays.js
@@ -2,7 +2,24 @@
 
 const array = [1,2,3,4,5];
 
+const heros=['thor','ironman','captain america']
 console.log(array[0]);
+const arr2= new Array(1,2,3);
+
 
 const array2= new Array(1,2,3,4);
+array2.push(10);
+console.log(array2);
+
+// includes checks for the value in the array
+
+console.log(array2.includes(10)); // true
+
+let newarr=[1,2,3,4];
+let newarr1= newarr.slice(0,2);
+console.log(newarr1)
+let newarr2=newarr.splice(0,2);
+console.log(newarr2);
+console.log(newarr);
+
 
