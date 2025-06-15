77123ba6f802076004e2f428a0f5eb24420a95ca
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-06-15 16:53:13 +0000

    obects_2 completed

--- 2_basics/objects_2.js
+++ 2_basics/objects_2.js
@@ -16,4 +16,38 @@ const regularUser={
         }
     }
 }
-console.log(regularUser.fullName);
+// console.log(regularUser.fullName.userfullname.firstname);
+
+const obj1={
+    1:"a",
+    2:"b",
+    
+}
+const obj2={3:"a",4:"b"}
+// const obj3={obj1,obj2}
+
+const obj3=Object.assign(obj1,obj2);
+// console.log(obj1===obj3);
+
+// obj3={...obj1,...obj2};
+
+const users=[
+    {
+        id:1,
+        email:"shan@gmail.com"
+    },
+    {
+        id:2,
+        email:"shanky@gmail.com"
+    },
+
+]
+
+console.log(tinderUser);
+
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty("name"));
+
