32fc53fda99225155eee4ec3f84bddc76c3c6f06
Shan <128463863+shanbhat2005@users.noreply.github.com>
2025-07-04 15:12:13 +0000

    scopes completed

--- 3_basics/02_scopes.js
+++ 3_basics/02_scopes.js
@@ -1,12 +1,45 @@
-let a = 10;
+
+if(true){
+   let a = 10;
 const b = 20;
 var c = 30;
+}
 
 
 
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const username="shan";
+
+    function two(){
+
+        const website="youtube";
+        // console.log(username);
+    }
+    // console.log(website);
+}
+
+if(true){
+    const username= "hitesh"
+    if(username==="hitesh"){
+         const website= "youtube"
+        //  console.log(username+ website);
+    }
+    // console.log(website)
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++
+
+function addone(value){
+return value+=1
+}
+console.log(addone(4));
+
+const addtwo = function(num){ return num +1;}
 
 
-
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(addtwo(4))
